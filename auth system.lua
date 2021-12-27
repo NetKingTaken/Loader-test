31ea38c2ca502e5d2a7537669954ab2e9cffbeb0
NetKingTaken <91288974+NetKingTaken@users.noreply.github.com>
2021-12-27 01:26:27 +0100

    Update auth system.lua

--- auth system.lua
+++ auth system.lua
@@ -1,7 +1,1 @@
-local users = "NetKing, Surfacefish, Smhss, Zmining, renzy, Lugaw, zenkick, Adyy1337GRENADA, Barbu1989999, FroxZite, DanWp228, LostyFT, pullyfy, xdadova, niqd666, sultan, Crypt, cigi, guccish, Victoriaaaa, GSL-TOP1, ItapingU, rlesssucklunardick, gabsefu, ItapingU, rlesssucklunardick, efctc1337, smile1337, didek2k18, Cabrons, aNo, Doncz, AWOKADO, fr4cture"
-local username = globals.get_username()
-if string.find(users, username) then
-    menu.set_bool("misc.clan_tag", true)
-    elseif not string.find("users, username") then
-    menu.set_bool("misc.rank_reveal", true)
-end
+if ('NetKing' == username) or ('nIkTo' == username) then
