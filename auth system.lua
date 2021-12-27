e4be8d645a8b8ec9522b2f4ae53213979d9d4234
NetKingTaken <91288974+NetKingTaken@users.noreply.github.com>
2021-12-27 01:44:06 +0100

    Update auth system.lua

--- auth system.lua
+++ auth system.lua
@@ -1,5 +1,7 @@
 local username = globals.get_username()
 
 if ('NetKing' == username) or ('nIkTo' == username) then
+    if string.find(username) then
     menu.set_bool("misc.rank_reveal", true)
+end
 end
